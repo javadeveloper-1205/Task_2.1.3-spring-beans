3624803f3944d5df2ea75f2f123673a322afaeb0
Greg <grygoriy23@gmail.com>
2020-11-09 08:46:58 -0600

    Error - I don't correctly realize beans

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,13 +1,20 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
-
     @Autowired
+    @Qualifier("dog")
     private Animal animal;
+//    public void setAnimal(@Qualifier("dog") Animal animal) {
+//        this.animal = animal;
+//    }
+
 
     public void whatAnimalSay() {
         System.out.println("Say:");
@@ -15,5 +22,10 @@ public class AnimalsCage {
         System.out.println("At:");
         System.out.println(new Timer().getTime());
         System.out.println("________________________");
+    }
+    @Autowired
+    @Qualifier("timer")
+    public Timer getTimer() {
+        return new Timer();
     }
 }
